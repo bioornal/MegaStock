3d457582810512e76544b5695d8852d6cba16f51
bioornal <spezialichristian@gmail.com>
2025-07-30 10:14:20 -0300

    mas colores

--- lib/productOptions.ts
+++ lib/productOptions.ts
@@ -10,7 +10,7 @@ export const BRANDS = [
 ];
 
 export const COLORS_BY_BRAND: { [key: string]: string[] } = {
-  Demobile: ["Amendola", "Cafe", "Blanco", "Amend/Off White", "Amend/Grafito", "Blanco/Rosa", "Nogueira", "Nogueira/Nude Prime", "Amend/Marsala", "Avena/Grafito", "Avena/Menta"],
+  Demobile: ["Amendola", "Cafe", "Blanco", "Amend/Off White", "Amend/Grafito", "Blanco/Rosa", "Nogueira", "Nogueira/Nude Prime", "Amend/Marsala", "Avena/Grafito", "Avena/Menta", "Nogueira/Preto","Amend/Nude Prime"],
   Mosconi: ["Blanco", "Venezia(Tissa)", "Choco", "Neb Natural(Bambu)", "Nebraska Gris(Grei)", "Carvalho Mezzo", "Carvalho Aserrado", "Tabaco", "Wengue", "Caoba", "Negro", "Helsinki", "Mendra"],
   // Agrega aquí más marcas y sus colores si es necesario
 };
